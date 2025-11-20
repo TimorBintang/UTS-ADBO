160d685d2021ed0e9fb46a4bb5eeff2cfbd00edd
TimorBintang <timorbintang79@gmail.com>
2025-11-20 21:16:14 +0800

    UTS ADBO-menentukan validasi cuti Karyawan

--- uts.js
+++ uts.js
@@ -1,78 +1,110 @@
-// --- 1. KELAS DASAR CUTI (INHERITANCE BASE) ---
-class Cuti {
-    constructor(nama, maxPerPengajuan) {
+class Cuti{
+    constructor(nama,pengajuan){
         this.nama = nama;
-        this.maxPerPengajuan = maxPerPengajuan; 
+        this.pengajuan = pengajuan;
+    }
+    max(){
+        return 0;
+    }
+    validasipengajuan(){
+        return "";
+    }
+    info(){
+        console.log(`\n==========${this.jenis}=========`)
+        console.log(`Nama Karyawan      = ${this.nama}`)
+        console.log(`jumlah pengajuan   = ${this.pengajuan}`)
+        console.log(`Maksimal pengajuan = ${this.max()}`)
+        console.log(`Validasi pengajuan = ${this.validasipengajuan()}\n`)
     }
 
-    validasi(hariDiajukan, sisaKuota) {
-        if (sisaKuota < hariDiajukan) {
-            console.log(`Gagal: Kuota ${this.nama} tidak cukup. (Sisa: ${sisaKuota})`);
-            return false;
+}
+
+
+class CutiTahunan extends Cuti{
+    constructor(nama,pengajuan){
+        super(nama,pengajuan)
+        this.jenis = "Cuti Tahunan";
+    }
+
+    max(){
+        return 12;
+    }
+
+    validasipengajuan(){
+        if(this.pengajuan <= this.max()){
+            return "pengajuan diterima"
         }
-
-        if (hariDiajukan > this.maxPerPengajuan) {
-            console.log(`Gagal: Max ${this.nama} per pengajuan adalah ${this.maxPerPengajuan} hari.`);
-            return false;
-        }
-
-        return true;
-    }
-}
-
-// --- 2. KELAS TURUNAN (INHERITANCE) ---
-class CutiTahunan extends Cuti {
-    constructor() { super("Cuti Tahunan", 12); } // Kuota max per pengajuan: 12 hari
-}
-
-class CutiSakit extends Cuti {
-    constructor() { super("Cuti Sakit", 2); } // Kuota max per pengajuan: 2 hari
-}
-
-class CutiMelahirkan extends Cuti {
-    constructor() { super("Cuti Melahirkan", 90); } // Kuota max per pengajuan: 90 hari
-}
-
-// --- 3. KELAS KARYAWAN (POLIMORFISME) ---
-class Karyawan {
-    constructor(nama) {
-        this.nama = nama;
-        this.kuota = {
-            "Cuti Tahunan": 12,
-            "Cuti Sakit": 2,
-            "Cuti Melahirkan": 90,
-        };
-    }
-
-    ajukanCuti(jenisCuti, hari) {
-        const namaCuti = jenisCuti.nama;
-        const sisa = this.kuota[namaCuti];
-
-        console.log(`\n--- Pengajuan ${namaCuti} oleh ${this.nama} (${hari} hari) ---`);
-        if (jenisCuti.validasi(hari, sisa)) {
-            this.kuota[namaCuti] -= hari;
-            console.log(`Disetujui! Sisa Kuota: ${this.kuota[namaCuti]} hari.`);
-        } else {
-            console.log(`Sistem menolak pengajuan: "Kuota anda tidak cukup untuk mengajukan cuti ini"`);
+        else{
+            return "Kuota anda tidak cukup untuk mengajukan cuti ini"
         }
     }
+    
 }
 
-// =========================================================
-//                  DEMO PENGGUNAAN
-// =========================================================
 
-const budi = new Karyawan("Budi");
-const tahunan = new CutiTahunan();
-const sakit = new CutiSakit();
+class CutiSakit extends Cuti{
+    constructor(nama,pengajuan){
+        super(nama,pengajuan)
+        this.jenis = "Cuti Sakit";
+    }
 
-// Skenario 1: Cuti Tahunan Berhasil
-budi.ajukanCuti(tahunan, 5); // Kuota Tahunan sisa 7 (12-5)
+    max(){
+        return 2;
+    }
 
-// Skenario 2: Cuti Sakit Gagal (Melebihi max per pengajuan)
-budi.ajukanCuti(sakit, 3); // Gagal, max 2 hari
+   validasipengajuan(){
+        if(this.pengajuan <= this.max()){
+            return "pengajuan diterima"
+        }
+        else{
+            return "Kuota anda tidak cukup untuk mengajukan cuti ini"
+        }
+    }
+}
 
-// Skenario 3: Cuti Tahunan Gagal (Kuota tidak cukup)
-budi.ajukanCuti(tahunan, 8); // Gagal, sisa kuota hanya 7
 
-console.log("\nKuota akhir Budi:", budi.kuota);
+class CutiMelahirkan extends Cuti{
+    constructor(nama,pengajuan){
+        super(nama,pengajuan)
+        this.jenis = "Cuti Melahirkan";
+    }
+
+    max(){
+        return 90;
+    }
+
+    validasipengajuan(){
+        if(this.pengajuan <= this.max()){
+            return "pengajuan diterima"
+        }
+        else{
+            return "Kuota anda tidak cukup untuk mengajukan cuti ini"
+        }
+    }
+}
+
+
+
+//Cuti Tahunan Diterima
+const ct1 = new CutiTahunan("Timor Bintang",11)
+ct1.info();
+
+//Cuti Tahunan Ditolak
+const ct2 =  new CutiTahunan("Timor Bintang",24)
+ct2.info();
+
+//Cuti Sakit Diterima
+const cs1 = new CutiSakit("Andi surandi",2)
+cs1.info();
+
+//Cuti Sakit Ditolak
+const cs2 = new CutiSakit("Andi Surandi",4)
+cs2.info();
+
+//Cuti Melahirkan Diterima
+const cm1 = new CutiMelahirkan("Siti Suriti",78)
+cm1.info();
+
+//Cuti Melahirkan Ditolak
+const cm2 = new CutiMelahirkan("Siti Suriti",100)
+cm2.info();
